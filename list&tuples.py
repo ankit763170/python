dad45db847c4d20a488338bd2d77e26c0ee611f1
ankit.jha <ankitjhakashyap82@gmail.com>
2024-10-29 19:20:01 +0530

    list vs tuples

---
+++ list&tuples.py
@@ -0,0 +1,23 @@
+# list
+
+mylist =["mango",55,89896,66]
+# print(mylist)
+# mylist[0]=98
+# print(mylist)
+
+# # mylist.sort(reverse=True)
+# # print(mylist)
+# mylist.append(555865)
+# newlist = mylist.copy()
+# # print(newlist)
+
+# print(newlist[1:4])
+# print(newlist[:4:2])
+# # tuples
+
+mytuples=(22,66,5557,66)
+# count =  mytuples.count(66)
+
+print(min(mytuples))
+
+# mytuples[0]=555

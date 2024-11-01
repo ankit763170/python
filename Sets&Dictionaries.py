a79537c240015cac5104835d745f4b6416dc03c4
ankit.jha <ankitjhakashyap82@gmail.com>
2024-11-01 20:28:21 +0530

    sets and dictionaries in python

---
+++ Sets&Dictionaries.py
@@ -0,0 +1,27 @@
+# set1={99,73829,45,32,42,999,999 }
+
+
+# # set1.add("ankit")
+# # print(set1)
+
+
+# # set1.remove("ankit")
+# # print(set1)
+
+
+# set2={55,89,"code",99}
+# print(set1.union(set2))
+# print(set2.intersection(set1))
+# print(set1 & set2)  #difference
+
+
+
+#dictionary
+dict1={
+    "key":"value",
+    "name" :"actualname"
+}
+# print(dict1)
+print(dict1.keys())
+print(dict1.values())
+print(dict1.items())
